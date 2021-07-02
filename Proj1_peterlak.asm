141f56b485e352f7d4b21cea8f52e4d1cbe1f2bc
LakePeterson <9petersonlake9@gmail.com>
2021-07-02 13:21:28 -0700

    Initial Commit

---
+++ Proj1_peterlak.asm
@@ -0,0 +1,31 @@
+Basic Logic and Arithmetic Program
+
+; Author: Lake Peterson
+; Last Modified: July 4, 2021
+; OSU email address: peterlak@oregonstate.edu
+; Course number/section: CS271 Section 001
+; Project Number: 1
+; Due Date: July 4, 2021
+; Description: The purpose of this assignment is to acquaint you with elementary MASM programming and integer arithmetic operations (CLO 3, 4).
+
+INCLUDE Irvine32.inc
+
+; (insert macro definitions here)
+
+; (insert constant definitions here)
+
+.data
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	Invoke ExitProcess,0	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
